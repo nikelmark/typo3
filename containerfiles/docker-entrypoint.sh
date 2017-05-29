590097b374ebe517dd5f8540d8902584a4ab825b
nikelmark <nikelmark@gmail.com>
2017-05-29 16:21:21 +0200

    Update docker-entrypoint.sh

--- containerfiles/docker-entrypoint.sh
+++ containerfiles/docker-entrypoint.sh
@@ -9,7 +9,7 @@ TP3_SITEURL=https://${MY_SRV}-${OPENSHIFT_BUILD_NAMESPACE}.${ENV_SUB_DOMAIN}
 export MY_SRV
 
 if [ ! -f  ${CONTENT_DIR}/$(basename $( echo ${TP3_FULL_FILE}|envsubst ) '.tar.gz')/index.php ]; then
-  tar xfz $( echo ${TP3_FULL_FILE} | envsubst) -C ${CONTENT_DIR}
+  tar -xf  $( echo ${TP3_VERS} | envsubst) -C ${CONTENT_DIR}
 fi
 
 exec /opt/rh/httpd24/root/sbin/httpd-scl-wrapper -D FOREGROUND
